7e401ba69d938fb5c47a251fe2c4d6c13fd365c2
Dan Codae <drcoda@gmail.com>
2018-09-05 19:46:05 -0400

    added some css styling and added divs to contain each row

--- src/App.js
+++ src/App.js
@@ -21,37 +21,50 @@ class App extends Component {
         <header className='App-header'>
           <h1 className='Calc-title'>Calculator X 2</h1>
         </header>
-        <div id='diplay'>{this.state.display}</div>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '1'})}>1</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '2'})}>2</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '3'})}>3</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '4'})}>4</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '5'})}>5</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '6'})}>6</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '7'})}>7</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '8'})}>8</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '9'})}>9</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '+'})}>+</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '-'})}>-</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '*'})}>*</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: this.state.display + '/'})}>/</button>
-        <button className='operater-button' onClick={() =>
-          this.historyDisplay()}>=</button>
-        <button className='operater-button' onClick={() =>
-          this.setState({display: ''})}>C</button>
+        <div id='display'>{this.state.display}
+          <button className='operator-button-clear' onClick={() =>
+            this.setState({display: ''})}>C</button>
+        </div>
+        <div id='top-row'>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '7'})}>7</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '8'})}>8</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '9'})}>9</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '/'})}>/</button>
+        </div>
+        <div id='middle-top'>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '4'})}>4</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '5'})}>5</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '6'})}>6</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '*'})}>*</button>
+        </div>
+        <div id='middle-bottom'>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '1'})}>1</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '2'})}>2</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '3'})}>3</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '-'})}>-</button>
+        </div>
+        <div id='bottom-row'>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '0'})}>0</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '.'})}>.</button>
+          <button className='operator-button' onClick={() =>
+            this.historyDisplay()}>=</button>
+          <button className='operator-button' onClick={() =>
+            this.setState({display: this.state.display + '+'})}>+</button>
+        </div>
         <div id='history'>
           {this.state.history.map((h, idx) => <div key={idx}>{h}</div>)}
         </div>
